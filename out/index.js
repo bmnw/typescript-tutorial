01c19f935c9a8e120c422a9e032be290c2808f5f
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-12-23 12:14:03 -0600

    added includes: src to tsconfig.json, created addNumbers function in index.ts

--- out/index.js
+++ out/index.js
@@ -1,3 +1,10 @@
 "use strict";
 let message = "Hello World!";
 console.log(message);
+// function to add two numbers
+const addNumbers = (a, b) => {
+    console.log("in addNumbers");
+    let sum = a + b;
+    return sum;
+}; // end addNumbers
+console.log(addNumbers(10, 15));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 let message : string = "Hello World!";
 console.log(message);
+
+// function to add two numbers
+const addNumbers = (a: number, b: number) => {
+    console.log("in addNumbers");
+    let sum: number = a + b;
+    return sum;
+} // end addNumbers
+
+console.log(addNumbers(10, 15));
