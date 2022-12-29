7d6144b029d09998d5b8d52a40c175f014d12f96
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-12-29 13:10:24 -0600

    created User interface

--- src/colorblocks.ts
+++ src/colorblocks.ts
@@ -4,3 +4,10 @@ function readyNow () {
     console.log('document ready');
 }
 
+interface User {
+    name: string,
+    favoriteColor: string,
+    newUser: boolean
+    favoriteShape?: string
+}
+
